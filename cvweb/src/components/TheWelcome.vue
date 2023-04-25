f3dbc736d5d6f846153150f7fe5ed28ca66883dc
Steffan <brandon.nunez@usach.cl>
2023-04-24 23:04:01 -0400

    inicializando pagina web

---
+++ cvweb/src/components/TheWelcome.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Education</template>
+      <p>Licenciado en Ciencias de la Ingeniería</p> 
+      <p>Titulado en Ingeniería de Ejecucion en computación e informática</p> 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Experience</template>
+      <p>full stack Developer, Tenlove Spa, 2021-2022</p>
+      <p>Software Developer, Talana linQ Spa, 2022 - present</p>
+      <p>FullStack Developer, proyectos personales, 2016 - presente</p>
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Skills</template>
+
+    Get official tools and libraries for your project:
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
+    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
+    you need more resources, we suggest paying
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
+    a visit.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Projects</template>
+
+    Pueden revisar mis proyectos en 
+    <a href="https://github.com/steffannunez/" target="_blank" rel="noopener">GitHub</a>
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Habilidades</template>
+    <p>Inglés Intermedio/Avanzado</p>
+    <p></p>
+  </WelcomeItem>
+</template>
